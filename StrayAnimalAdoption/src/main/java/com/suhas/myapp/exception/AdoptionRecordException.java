7b769c19dcc2ffcc6f88c835fbf4294ea7fbc490
SuhasMC <chandrashekar.su@husky.neu.edu>
2020-07-07 19:52:51 -0400

    Added custom Exceptions

---
+++ StrayAnimalAdoption/src/main/java/com/suhas/myapp/exception/AdoptionRecordException.java
@@ -0,0 +1,10 @@
+package com.suhas.myapp.exception;
+
+public class AdoptionRecordException extends Exception {
+	public AdoptionRecordException(String message)	{
+		super("AdoptionRecordException- "+message);
+	}
+	public AdoptionRecordException(String message, Throwable cause){
+		super("AdoptionRecordException- "+message, cause);
+	}
+}
